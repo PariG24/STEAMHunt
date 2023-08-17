b0e1b357fa355f86b32921cf17196a9f097b9c1f
Pari Gulati <pgulati182@gmail.com>
2023-08-17 10:07:02 -0500

    Initial Commit

---
+++ STEAM Hunt APP/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  STEAM Hunt APP
+//
+//  Created by Pari Gulati on 8/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ STEAM Hunt APP/STEAM_Hunt_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  STEAM_Hunt_APPApp.swift
+//  STEAM Hunt APP
+//
+//  Created by Pari Gulati on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct STEAM_Hunt_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
